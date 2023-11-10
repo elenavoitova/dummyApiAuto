51478b4c1e898dcb19c5ba911a59bcc64f99d6dc
elenavoitova <elena.voitova.lb@gmail.com>
2023-11-10 12:20:13 +0200

    Create EmployeeResponse.java

---
+++ src/main/java/core/EmployeeResponse.java
@@ -0,0 +1,39 @@
+package core;
+
+import core.model.Employee;
+
+import java.util.List;
+
+public class EmployeeResponse {
+
+    private String status;
+
+    private List<Employee> data;
+
+    private String message;
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Employee> getData() {
+        return data;
+    }
+
+    public void setData(List<Employee> data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
